2fe00978139a6612fbdc39b5e0557a5bc936b1ee
Balint Handler <handlerbalint@gmail.com>
2021-04-22 20:42:21 +0200

    added menu with new, save, load, delete, exit options

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,3 +1,4 @@
+require 'json'
 require 'pry'
 
 def random_word(path)
@@ -11,20 +12,27 @@ def random_word(path)
 end
 
 def get_input
-  letter = ''
+  guess = ''
   loop do
     puts 'Type your guess:'
-    letter = gets.chomp.downcase
-    break if /^[a-z]$/.match(letter)
+    guess = gets.chomp.downcase
+
+    if guess == 'menu'
+      return 'menu'
+      break
+    end
+
+    break if /^[a-z]$/.match(guess)
     puts "Enter a single letter!"
+
   end
-  letter
+  guess
 end
 
 def display_secret(secret_word, arr = [])
-  secret_word.split('').each_with_index { |letter,index|
+  secret_word.split('').each_with_index { |guess,index|
     if arr.any? {|i| i == index}
-      print letter
+      print guess
     else
       print "_"
     end
@@ -139,50 +147,219 @@ def show_hangman(turns)
 end
 
 class Game 
-  def initialize(secret, solved = [], turns = 10)
-    @secret = secret
-    @solved = solved
-    @turns = turns
+
+  def initialize
+    @secret = random_word('5desk.txt')
+    @solved_indexes = []
+    @turns = 10
+    @found = []
+    @missed = []
     @gameover = false
+    
   end
 
   def start_game
     puts "Hangman game initialized\n\n"
     puts "Try to guess the secret word below!"
-    display_secret(@secret, @solved)
-    puts "You can miss 9 times, 10th will be game over."
+    display_secret(@secret, @solved_indexes)
+    puts "You have 10 guess to miss."
+    puts "To enter the Game Menu, type menu"
+  end
+
+  def menu
+    puts "---------  GAME MENU  ---------"
+    puts "Available commands:"
+    puts "  new   - start new game"
+    puts "  save   - save current game"
+    puts "  load   - load saved game"
+    puts "  delete - delete saved game"
+    puts "  exit   - exit menu"
+    puts "-------------------------------"
+    command = ''
+    until /^new$|^save$|^load$|^delete$|^exit$/.match(command)
+      puts "Type your command:"
+      command = gets.chomp
+    end
+    if command == 'new'
+      puts
+      puts "Starting New Game..."
+      command_new()
+    end
+    if command == 'save'
+      command_save()
+    end
+    if command == 'load'
+      command_load()
+      return 'load'
+    end
+    if command == 'delete'
+      command_delete()
+    end
+
+  end
+
+  def command_new
+
+    @secret = random_word('5desk.txt')
+    @solved_indexes = []
+    @turns = 10
+    @found = []
+    @missed = []
+    @gameover = false
+    start_game()
+  end
+
+  def command_save
+    savegame()
+    puts
+    puts "Game Saved"
+    puts
+    
+  end
+
+  def command_load
+    loadgame()
+    puts
+    puts "Game Loaded"
+    puts
+    
+  end
+
+  def command_delete
+    deletegame
+    puts
+    puts "Saved game deleted"
+    puts
+    
   end
 
   def game_logic
+    puts
+    start_game()
+
     until @gameover
-      letter = get_input()
-      evaluate(@secret, letter)
+      guess = get_input()
+      if guess == 'menu'
+        menu = menu()
+        unless menu == 'load'
+          next
+        end
+      end
+      puts
+      evaluate(@secret, guess)
       show_hangman(@turns)
-      display_secret(@secret, @solved)
+      display_secret(@secret, @solved_indexes)
+      display_guess(guess)
+      puts
+      display_found(@found)
+      display_missed(@missed)
+      display_turns(@turns)
+      puts
 
       if @turns == 0
         @gameover = true
+        lose_message()
+      elsif @secret.length == @solved_indexes.length
+        @gameover = true
+        win_message()
       end
     end
 
+  end
+
+
+  def display_turns(turns)
+    puts "Turns left: #{turns}"
+  end
+
+  def display_guess(guess)
+    puts "Your guess: #{guess}"
+  end
+
+  def display_found(found) 
+    print "Letters found:"
+    found.each_with_index { |e, index|
+      unless index == found.length - 1
+        print " #{e},"
+      else
+        print " #{e}."
+      end
+    }
+    puts
+  end
+
+  def display_missed(missed) 
+    print "Letters missed:"
+    missed.each_with_index { |e, index|
+      unless index == missed.length - 1
+        print " #{e},"
+      else
+        print " #{e}."
+      end
+    }
+    puts
+  end
+
+  def win_message
+    puts "CONGRATS!"
+    puts "You won!"
+  end
+
+  def lose_message
     puts "GAME OVER"
     puts "Out of guesses"
+    puts "Secret word was: #{@secret}"
   end
 
-  def evaluate(secret,letter)
-    arr = secret.downcase.split('')
-    if arr.none? {|i| i == letter}
-      @turns -= 1
-    else
-      arr.each_with_index {|secret_letter, index|
-      if secret_letter == letter 
-        @solved << index
+  def evaluate(secret,guess)
+    secret_arr = secret.downcase.split('')
+    success = false
+
+    secret_arr.each_with_index {|secret_letter, index|
+    if secret_letter == guess 
+      success = true
+      if @solved_indexes.none? {|e| e == index}
+        @solved_indexes << index
       end
-      }
+      if @found.none? {|i| i == guess}
+        @found << guess
+      end
     end
-    puts "Turns left: #{@turns}"
+    }
+
+    #   if secret_arr.none? {|i| i == guess}
+    # else
+      if !success 
+        @turns -= 1
+        @missed << guess
+      
+    end
+    
   end
 
+  def savegame
+    obj = {}
+    instance_variables.map {|var|
+      obj[var] = instance_variable_get(var)
+    }
+    file = File.open('hangman.json', 'w+')
+    file.puts JSON.dump obj
+    file.close
+    
+  end
+
+  def loadgame
+    file = File.read('hangman.json')
+    obj = JSON.parse file
+    obj.keys.each { |key|
+      instance_variable_set(key, obj[key])
+    }
+  end
+
+  def deletegame
+    file = File.open('hangman.json', 'w+')
+    file.close
+  end
 end
 
 
@@ -190,7 +367,7 @@ end
 
 
 
-game = Game.new(random_word('5desk.txt'))
-game.start_game
-
+game = Game.new()
 game.game_logic
+
+
